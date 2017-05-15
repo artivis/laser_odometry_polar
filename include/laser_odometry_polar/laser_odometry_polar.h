9f5f9445604dc4d33ad903c294629d26600ff3d0
Jeremie Deray <jeremie.deray@pal-robotics.com>
2017-05-15 17:34:52 +0200

    project upload

---
+++ include/laser_odometry_polar/laser_odometry_polar.h
@@ -0,0 +1,55 @@
+#ifndef _LASER_ODOMETRY_POLAR_LASER_ODOMETRY_POLAR_H_
+#define _LASER_ODOMETRY_POLAR_LASER_ODOMETRY_POLAR_H_
+
+#include <laser_odometry_core/laser_odometry_core.h>
+
+#include <polar_scan_matcher/polar_match.h>
+
+#include <laser_odometry_polar/LaserOdometryPolarParameters.h>
+
+namespace laser_odometry
+{
+
+class LaserOdometryPolar : public LaserOdometryBase
+{
+  using Base = LaserOdometryBase;
+
+  using Parameters = laser_odometry_polar::LaserOdometryPolarParameters;
+  using ParametersPtr = std::shared_ptr<Parameters>;
+
+public:
+
+  LaserOdometryPolar()  = default;
+  ~LaserOdometryPolar() = default;
+
+  OdomType odomType() const noexcept override;
+
+protected:
+
+  bool process_impl(const sensor_msgs::LaserScanConstPtr& laser_msg,
+                    const Transform& prediction) override;
+
+protected:
+
+  ParametersPtr params_ptr_;
+  double kf_dist_linear_sq_;
+
+  std::shared_ptr<PMScan> prev_scan_;
+
+  PolarMatcher polar_matcher_;
+
+  void convert(const sensor_msgs::LaserScanConstPtr& scan_msg,
+               std::shared_ptr<PMScan>& psm_scan);
+
+  bool configureImpl() override;
+
+  bool initialize(const sensor_msgs::LaserScanConstPtr& scan_msg) override;
+
+  void updateLaserPose();
+
+  bool isKeyFrame(const Transform& increment) override;
+};
+
+} /* namespace laser_odometry */
+
+#endif /* _LASER_ODOMETRY_POLAR_LASER_ODOMETRY_POLAR_H_ */
---
+++ src/laser_odometry_polar.cpp
@@ -0,0 +1,127 @@
+#include <laser_odometry_polar/laser_odometry_polar.h>
+
+#include <laser_odometry_core/laser_odometry_utils.h>
+
+#include <pluginlib/class_list_macros.h>
+
+namespace laser_odometry {
+
+// convert from cm to m
+constexpr double CM_TO_M = 100.0;
+constexpr double READING_ERROR = 999.9;
+
+OdomType LaserOdometryPolar::odomType() const noexcept
+{
+  return OdomType::Odom2D;
+}
+
+bool LaserOdometryPolar::configureImpl()
+{
+  params_ptr_ = std::make_shared<Parameters>(private_nh_);
+  params_ptr_->fromParamServer();
+
+  kf_dist_linear_sq_ = params_ptr_->kf_dist_linear;
+  kf_dist_linear_sq_ *= kf_dist_linear_sq_;
+
+  ROS_INFO_STREAM("LaserOdometryPolar parameters:\n" << *params_ptr_);
+
+  return true;
+}
+
+bool LaserOdometryPolar::process_impl(const sensor_msgs::LaserScanConstPtr& laser_msg,
+                                      const Transform& prediction)
+{
+  std::shared_ptr<PMScan> current_scan_ = std::make_shared<PMScan>(laser_msg->ranges.size());
+
+  convert(laser_msg, current_scan_);
+
+  current_scan_->rx = prediction.translation()(0);
+  current_scan_->ry = prediction.translation()(1);
+  current_scan_->th = utils::getYaw(prediction.linear());
+
+  try
+  {
+    polar_matcher_.pm_psm(prev_scan_.get(), current_scan_.get());
+  }
+  catch(const int err)
+  {
+    ROS_WARN("Error %i in polar scan matching.", err);
+    prev_scan_ = current_scan_;
+    return false;
+  }
+
+  increment_.translation()(0) =  current_scan_->ry / CM_TO_M;
+  increment_.translation()(1) = -current_scan_->rx / CM_TO_M;
+  increment_.linear() = utils::matrixYaw(current_scan_->th);
+
+  return true;
+}
+
+void LaserOdometryPolar::convert(const sensor_msgs::LaserScanConstPtr& scan_msg,
+                                 std::shared_ptr<PMScan>& psm_scan)
+{
+  psm_scan->rx = 0;
+  psm_scan->ry = 0;
+  psm_scan->th = 0;
+
+  for (int i = 0; i < scan_msg->ranges.size(); ++i)
+  {
+    int reading_bad = 0;
+
+    if (scan_msg->ranges[i] == 0)
+    {
+      psm_scan->r[i] = READING_ERROR;
+      reading_bad = 1;
+    }
+    else
+    {
+      psm_scan->r[i] = scan_msg->ranges[i] * CM_TO_M;
+      psm_scan->x[i] = psm_scan->r[i] * polar_matcher_.pm_co[i];
+      psm_scan->y[i] = psm_scan->r[i] * polar_matcher_.pm_si[i];
+    }
+
+    psm_scan->bad[i] = reading_bad;
+  }
+
+  polar_matcher_.pm_median_filter  (psm_scan.get());
+  polar_matcher_.pm_find_far_points(psm_scan.get());
+  polar_matcher_.pm_segment_scan   (psm_scan.get());
+}
+
+bool LaserOdometryPolar::initialize(const sensor_msgs::LaserScanConstPtr& scan_msg)
+{
+  polar_matcher_.PM_L_POINTS         = scan_msg->ranges.size();
+
+  polar_matcher_.PM_FOV              = (scan_msg->angle_max - scan_msg->angle_min) * PM_R2D;
+  polar_matcher_.PM_MAX_RANGE        = scan_msg->range_max * CM_TO_M;
+
+  polar_matcher_.PM_TIME_DELAY       = 0.00;
+
+  polar_matcher_.PM_MIN_VALID_POINTS = params_ptr_->min_valid_points;
+  polar_matcher_.PM_SEARCH_WINDOW    = params_ptr_->search_window;
+  polar_matcher_.PM_MAX_ERROR        = params_ptr_->max_error * CM_TO_M;
+
+  polar_matcher_.PM_MAX_ITER         = params_ptr_->max_iterations;
+//  polar_matcher_.PM_MAX_ITER_ICP     = params_ptr_->max_iterations_icp;
+  polar_matcher_.PM_STOP_COND        = params_ptr_->stop_condition * CM_TO_M;
+//  polar_matcher_.PM_STOP_COND_ICP    = params_ptr_->stop_condition_icp * CM_TO_M;
+
+  polar_matcher_.pm_init();
+
+  prev_scan_ = std::make_shared<PMScan>(scan_msg->ranges.size());
+
+  return true;
+}
+
+bool LaserOdometryPolar::isKeyFrame(const Transform& increment)
+{
+  if (std::fabs(utils::getYaw(increment.rotation())) > params_ptr_->kf_dist_angular) return true;
+
+  if (increment.translation().head<2>().squaredNorm() > kf_dist_linear_sq_) return true;
+
+  return false;
+}
+
+} /* namespace laser_odometry */
+
+PLUGINLIB_EXPORT_CLASS(laser_odometry::LaserOdometryPolar, laser_odometry::LaserOdometryBase);
